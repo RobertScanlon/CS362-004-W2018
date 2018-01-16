5d210fb831e4f50cf71c019c1bac159df1112d12
Robert Scanlon <scanlonr@oregonstate.edu>
2018-01-16 06:35:25 -0500

    added cards.h

---
+++ projects/scanlonr/dominion/cards.h
@@ -0,0 +1,5 @@
+// play smithy
+void playSmithy(int currentPlayer, struct gameState *state, int handPos);
+
+// play adventurer
+void playAdventurer(int currentPlayer, struct gameState *state, int drawnTreasure, )
