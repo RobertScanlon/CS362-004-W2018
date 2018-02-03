4196735dd826734f93da7d45dff98c2b919fc3bf
Robert Scanlon <scanlonr@oregonstate.edu>
2018-02-03 16:49:47 -0500

    makefile changes

--- projects/scanlonr/dominion/cardtest1.c
+++ projects/scanlonr/dominion/cardtest1.c
@@ -8,8 +8,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int
-main()
+void testSmithy()
 {
 	// testing is done though cardEffect which is assumed to operate correctly
 	// for testing smithy we are only concered with local variables
@@ -20,9 +19,51 @@ main()
 	int choice3 = 0;
 	int handPos = 0;
 	int bonus = 0;
+	int player = 0;
+	struct gameState testG;
+	struct gameState refG;
+	int retValue = -5;
+	int seed = 1;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+				 sea_hag, tribute, smithy, council_room};
 
+	initializeGame(2, k, seed, &refG);
+	memcpy(&testG, &refG, sizeof(struct gameState));
 
-	// test smithy when player has empty deck
+	// set smithy as card in hand[handPos]
+	testG.hand[player][testG.handCount[player] - 1] = smithy;
+	refG.hand[player][refG.handCount[player] - 1] = smithy;
 
+	// set the to contain only 10 copper
+	for (int i = 0; i < 10; i++) {
+		testG.deck[player][i] = copper;
+		refG.deck[player][i] = copper;
+	}
+	testG.deckCount[player] = 10;
+	refG.deckCount[player] = 10;
+
+	retValue = cardEffect(card, choice1, choice3, choice3, &testG, handPos, &bonus);
+
+	// assert that cardEffect returns 0
+	myAssert(retValue, 0, "playSmithy() returns 0 from cardEffect()");
+
+	// assert that hand has 2 more cards
+	myAssert(testG.handCount[player], refG.handCount[player] + 2, "Playing smithy increased handCount by 2 (draw 3, discard smithy)");
+
+	// assert that new cards came from players deck
+	int c = 1;
+	for (int i = refG.handCount[player]; i < testG.handCount[player]; i++) {
+		if (testG.hand[player][i] != copper) {
+			c = 0;
+		}
+	}
+	myAssert(c, 1, "New Cards came from players deck");
+
+}
+
+int
+main()
+{
+	testSmithy();
 	return 0;
 }
--- projects/scanlonr/dominion/unittest4.c
+++ projects/scanlonr/dominion/unittest4.c
@@ -7,10 +7,60 @@
 #include "myAssert.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
+
+void testCompare()
+{
+	int zero = 0;
+	int neg1 = -1;
+	int one = 1;
+	int two = 2;
+	int min = INT_MIN;
+	int max = INT_MAX;
+
+	// -1, -1 => 0
+	myAssert(compare((void*)&neg1, (void*)&neg1), 0, "-1 = -1 returns 0");
+
+	// 0, 0 => 0
+	myAssert(compare((void*)&zero, (void*)&zero), 0, "0 = 0 returns 0");
+
+	// 1, 1 => 0
+	myAssert(compare((void*)&one, (void*)&one), 0, "1 = 1 returns 0");
+
+	// -1, 0 => -1
+	myAssert(compare((void*)&neg1, (void*)&zero), -1, "-1 < 0 returns -1");
+
+	// 0, 1 => -1
+	myAssert(compare((void*)&zero, (void*)&one), -1, "0 < 1 returns -1");
+
+	// 1, 2 => -1
+	myAssert(compare((void*)&one, (void*)&two), -1, "1 < 2 returns -1");
+
+	// 0, -1 => 1
+	myAssert(compare((void*)&zero, (void*)&neg1), 1, "0 > -1 returns 1");
+
+	// 1, 0 => 1
+	myAssert(compare((void*)&one, (void*)&zero), 1, "1 > 0 returns 1");
+
+	// 2, 1 => 1
+	myAssert(compare((void*)&two, (void*)&one), 1, "2 > 1 returns 1");
+
+	// INT_MIN, INT_MAX => -1
+	myAssert(compare((void*)&min, (void*)&max), -1, "INT_MIN < INT_MAX returns -1");
+
+	// INT_MAX, INT_MIN => 1
+	myAssert(compare((void*)&max, (void*)&min), 1, "INT_MAX > INT_MIN returns 1");
+
+	// INT_MIN, INT_MIN => 0
+	myAssert(compare((void*)&min, (void*)&min), 0, "INT_MIN = INT_MIN returns 0");
+
+	// INT_MAX, INT_MAX => 0
+	myAssert(compare((void*)&max, (void*)&max), 0, "INT_MAX = INT_MAX returns 0");
+}
 
 int
 main()
 {
-	fprintf(stderr, "Hello, unittest4\n");
+	testCompare();
 	return 0;
 }
