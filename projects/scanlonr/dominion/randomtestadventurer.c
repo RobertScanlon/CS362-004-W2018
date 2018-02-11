4ee7dd9ec1319cdf77f8cfbfe319939860c31e78
Robert Scanlon <scanlonr@oregonstate.edu>
2018-02-11 11:55:59 -0500

    adventurer oracle test

--- projects/scanlonr/dominion/randomtestadventurer.c
+++ projects/scanlonr/dominion/randomtestadventurer.c
@@ -9,16 +9,28 @@
 #include <stdio.h>
 #include <string.h>
 
-void adventurerChecker(struct gameState *post, int player, int drawnT, int card, int z, int *tmpHand) {
+void adventurerChecker(struct gameState *post) {
     // copy gamstate into 'pre' so it can be compared to its state
     // before cardEffect is called
     struct gameState pre;
     memcpy(&pre, post, sizeof(struct gameState));
 
+    int player = post->whoseTurn;
+
     int numTreasure = 0;
     int currentCard = 0;
     int tmp[MAX_HAND];
     int tmpPos = 0;
+
+    int retValue;
+
+    int choice1 = 0;
+    int choice2 = 0;
+    int choice3 = 0;
+    int handPos = 0;
+    int bonus = 0;
+
+    retValue = cardEffect(adventurer, choice1, choice2, choice3, post, handPos, &bonus);
 
     while (numTreasure < 2) {
         currentCard = drawCard(player, &pre);
@@ -33,11 +45,24 @@ void adventurerChecker(struct gameState *post, int player, int drawnT, int card,
         pre.discard[player][pre.discardCount[player]] = tmp[i];
         pre.discardCount[player]--;
     }
+    myAssert(0, retValue, "cardEffect returns 0", 1);
     myAssert(0, memcmp(&pre, post, sizeof(struct gameState)), "Pre and Post states are equal", 1);
+}
+
+void checkOracle()
+{
+    struct gameState G;
+    int seed = 1;
+    int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+                 sea_hag, tribute, smithy, council_room};
+
+    initializeGame(2, k, seed, &G);
+    adventurerChecker(&G);
 }
 
 int
 main()
 {
+    checkOracle();
 	return 0;
 }
