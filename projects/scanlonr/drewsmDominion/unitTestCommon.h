e9ef15b8044d43670fab22a43605ecb111cf057b
Robert Scanlon <scanlonr@oregonstate.edu>
2018-02-25 14:16:01 -0500

    added drewsm code

---
+++ projects/scanlonr/drewsmDominion/unitTestCommon.h
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   unitTestCommon.h
+ * Author: mel
+ *
+ * Created on January 31, 2018, 8:20 PM
+ */
+
+#ifndef UNITTESTCOMMON_H
+#define UNITTESTCOMMON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <string.h>
+#include <stdio.h>
+#include "interface.h"
+
+//Specify the number of rounds for random tests
+#define ROUNDS 100
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+
+//Global variable
+
+//function definitions
+int myCompare(int, int);
+void printResult(int, char*, int);
+void printSummary(int);
+
+#endif /* UNITTESTCOMMON_H */
+
